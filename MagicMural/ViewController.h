293abd4159035ad8266f3a7b72d59779ebf8b1d1
gmarquez <gdmarquez.90@gmail.com>
2015-11-24 12:33:39 -0300

    Initial Commit

---
+++ MagicMural/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MagicMural
+//
+//  Created by Ger on 11/24/15.
+//  Copyright © 2015 Ger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MagicMural/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  MagicMural
+//
+//  Created by Ger on 11/24/15.
+//  Copyright © 2015 Ger. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
